fecf18ea405efebc4631420cdcb61cdde9aff24c
Anusree <anilkumaranusree113@gmail.com>
2024-11-10 11:22:27 +0530

    changed path of errorHandler in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors') //to allow cross-origin resource sharing
 var cookieParser = require('cookie-parser');//to get cookies 
 const path = require("path")
 const routes = require("./routes");
-const { errorHandler } = require('./middlewares/errorHandler.middleware.js');
+const { errorHandler } = require('./middlewares/errorhandler.middleware');
 const passport = require("passport");
 const { localStrategy, jwtStrategy } = require('./config/passport.config');
 
